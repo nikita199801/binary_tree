9e9040163febda31d04ab160557277f843b36e2a
Nikita <nikita199801@gmail.com>
2019-03-24 00:15:53 +0300

    add new

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ from bintree import BinTree
 
 
 new_tree=BinTree()
-new_treee=BinTree()
-a=["f","w","b","e","a","t","u","l","d","z","s","q"]
-b=["ф","а","в","б","е","ц","с"]
-for x in b:
-    new_tree.insert(x)
-for x in a:
-    new_treee.insert(x)
-new_tree.printTree(new_tree.getRoot())
-print("-"*20)
-new_tree.printWrds(new_tree.getRoot())
-print("-"*20)
-new_treee.printTree(new_treee.getRoot())
-print("-"*20)
-new_treee.printWrds(new_treee.getRoot())
+while True:
+    choose = int(input("1. Ввести дерево \n"
+          "2. Вывести дерево\n"
+          "3. Вывести все возможные слова\n"
+          "0. Выход\n"))
+    if choose == 1:
+        val=""
+        while val !=" ":
+            val=input("Введите узел: ")
+            if val == " ":
+                break
+            else:
+                new_tree.insert(val)
+    elif choose == 2:
+        new_tree.printTree(new_tree.getRoot())
+    elif choose == 3:
+        new_tree.printWrds(new_tree.getRoot())
+    elif choose == 0:
+        break
+
+
+# a=["f","w","b","e","a","t","u","l","d","z","s","q"]
+# b=["ф","а","в","б","е","ц","с"]
 
 
 
